7437aa8f03fa514df5e32e7ceebdd99400aef7a3
Fedor Eronin <eroninf@icloud.com>
2022-08-04 18:33:59 +0300

    add search bar

--- CoctailTestTask/Views/ViewController.swift
+++ CoctailTestTask/Views/ViewController.swift
@@ -16,6 +16,8 @@ protocol AnyView {
 
 
 class CoctailViewController: ASDKViewController<ASDisplayNode>, AnyView {
+    var searchTimer: Timer?
+    
     func update(with users: [Coctail]) {
         
     }
@@ -30,34 +32,94 @@ class CoctailViewController: ASDKViewController<ASDisplayNode>, AnyView {
     
     let coctails: [Coctail] = []
     
+    private let searchBar: SearchField
     private let coctailsNode: CoctailCollectionNode
     
-
     override init() {
         let node = ASDisplayNode()
         self.coctailsNode = CoctailCollectionNode(coctails: coctails)
-        coctailsNode.reloadData()
+        searchBar = SearchField()
+        
         super.init(node: node)
         node.backgroundColor = .yellow
         self.node.automaticallyManagesSubnodes = true
         self.node.layoutSpecBlock = {_,_ in
             let collection = ASInsetLayoutSpec(insets: UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16), child: self.coctailsNode)
-            
-            return collection
+            let stac = ASStackLayoutSpec(direction: .vertical,
+                                         spacing: 23,
+                                         justifyContent: .start,
+                                         alignItems: .start,
+                                         children: [collection, self.searchBar])
+            return stac
         }
     }
     
-    override func viewDidLoad() {
-        NetwotkManager.fetchData(searchString: "martini") {
+    private func fetchData(searchString: String) {
+        NetwotkManager.fetchData(searchString: searchString) {
             guard let coctails = $0 else { return }
             self.coctailsNode.coctails = coctails
-            print(coctails)
             self.coctailsNode.reloadData()
         }
     }
     
+    override func viewDidLoad() {
+        searchBar.textField.delegate = self
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class SearchField: ASDisplayNode {
+    var searchTimer: Timer?
+    let textField: ASEditableTextNode
+    let background: ASDisplayNode
+    
+    override init() {
+        textField = ASEditableTextNode()
+        textField.attributedText = NSMutableAttributedString(string: "KLR:GJ:OIDFJG:LKDFJGL")
+    
+        background = ASDisplayNode()
+        background.style.preferredSize = CGSize(width: 200, height: 28)
+        background.backgroundColor = .gray
+        
+        super.init()
+        self.automaticallyManagesSubnodes = true
+    }
+    
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        let paddingTextField = ASInsetLayoutSpec(insets: UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16), child: textField)
+        let mainStack = ASOverlayLayoutSpec(child: background, overlay: paddingTextField)
+        
+        
+        
+        return mainStack
+    }
+    
+}
+extension CoctailViewController: ASEditableTextNodeDelegate {
+    func editableTextNodeDidUpdateText(_ editableTextNode: ASEditableTextNode){
+        
+        if searchTimer != nil {
+            searchTimer?.invalidate()
+            searchTimer = nil
+        }
+        
+        searchTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(searchText), userInfo: searchBar.textField.attributedText?.string, repeats: false)
+        
+    }
+    
+    @objc func searchText() {
+        let text = searchBar.textField.attributedText
+        guard let text = text?.string else { return }
+        fetchData(searchString: text)
+        
+         
     }
 }
 
