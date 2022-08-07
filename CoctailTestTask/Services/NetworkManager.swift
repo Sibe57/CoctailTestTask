fc6578fcb057808d49241b11c5b8d5702d183acb
Fedor Eronin <eroninf@icloud.com>
2022-08-07 21:25:03 +0300

    add dispatchGroup

--- CoctailTestTask/Services/NetworkManager.swift
+++ CoctailTestTask/Services/NetworkManager.swift
@@ -6,53 +6,51 @@
 //
 
 import Alamofire
+import Darwin
 
 class NetwotkManager {
     
     static func fetchData(searchString: String,
                           completion: @escaping ([Coctail]?) -> Void) {
-        guard !searchString.isEmpty else {
-            completion(nil)
-            return
-        }
         
         var result: Set<Coctail> = []
-        
-        var numberOfRequestsDone = 0 {
-            willSet {
-                if newValue == 2 {
-                    completion(Array(result))
-                    return
-                }
-            }
-        }
         
         let searchByNameURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=\(searchString)"
         let searchByIngridientsURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=\(searchString)"
         
-        createRequest(to: searchByNameURL)
-        createRequest(to: searchByIngridientsURL)
+        let requestGroup = DispatchGroup()
+        
+        let queue = DispatchQueue(label: "NetworkGroup", attributes: .concurrent)
         
         func createRequest(to string: String) {
+            requestGroup.enter()
             AF.request(string)
                 .responseDecodable(of: [String: Set<Coctail>?].self) { data in
                     switch data.result {
                     case .success(let value):
+                        print("request")
                         for i in value {
-                            DispatchQueue.main.async {
-                                if let coctails = i.value {
-                                    result = result.union(coctails)
-                                }
-                                numberOfRequestsDone += 1
+                            if let coctails = i.value {
+                                result = result.union(coctails)
+                                requestGroup.leave()
                             }
                         }
                     case .failure(let error):
-                        DispatchQueue.main.async {
-                            numberOfRequestsDone += 1
-                        }
                         print(error)
+                        requestGroup.leave()
                     }
                 }
+        }
+        
+        queue.async(group: requestGroup) {
+            createRequest(to: searchByNameURL)
+        }
+        
+        queue.async(group: requestGroup) {
+            createRequest(to: searchByIngridientsURL)
+        }
+        requestGroup.notify(queue: .main) {
+            completion(Array(result))
         }
     }
 }
