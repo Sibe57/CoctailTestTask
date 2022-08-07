c7c1fe3db789e63baf9b5e2ec98ad72405852805
Fedor Eronin <eroninf@icloud.com>
2022-08-07 20:35:14 +0300

    add DataFetchError enum for describe errors

---
+++ CoctailTestTask/Entities/Errors.swift
@@ -0,0 +1,14 @@
+//
+//  Errors.swift
+//  CoctailTestTask
+//
+//  Created by Федор Еронин on 07.08.2022.
+//
+
+import Foundation
+
+
+enum DataFetchError: String {
+    case emptyTextField = "Empty text field"
+    case fetchDataError = "Data fetching error"
+}
